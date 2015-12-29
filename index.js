32c00a17cfc62fe9de0c6942fa588be48a5b6d2d
Joe <joedaniels29@me.com>
2015-12-28 21:57:46 -0500

    Fixes allow nesting of add ons

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ module.exports = {
   blueprintsPath: function() {
     return path.join(__dirname, 'blueprints');
   },
+  
+  
+   included: function(app, parentAddon) {
+     // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+      this.app = app = app.app;
+    }
+  }
+  
   treeForStyles: function() {
+     // see: https://github.com/ember-cli/ember-cli/issues/3718
+    var app = this.app;
+    if (typeof app.import !== 'function' && app.app) {
+      app = this.app.app;
+    }
     var neatPath = path.join(this.app.bowerDirectory, 'neat', 'app', 'assets', 'stylesheets');
     var neatTree = new Funnel(this.treeGenerator(neatPath), {
       srcDir: '/',
